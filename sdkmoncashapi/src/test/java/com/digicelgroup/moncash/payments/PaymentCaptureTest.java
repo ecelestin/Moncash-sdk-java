1918319b4357d745864cdca7f34b2256d4dbc49a
Emmanuel Suy <suyemmanuel@gmail.com>
2019-05-23 09:04:48 -0400

    First commit Java-moncash-sdk

---
+++ sdkmoncashapi/src/test/java/com/digicelgroup/moncash/payments/PaymentCaptureTest.java
@@ -0,0 +1,52 @@
+package com.digicelgroup.moncash.payments;
+
+import com.digicelgroup.moncash.APIContext;
+import com.digicelgroup.moncash.http.Constants;
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+public class PaymentCaptureTest {
+
+    private static final Logger logger = Logger
+            .getLogger(PaymentCaptureTest.class.getName());
+
+    @Test
+    public void captureByTransactionId() throws Exception {
+        PaymentCapture paymentCapture = new PaymentCapture();
+        APIContext apiContext = new APIContext(CredentialTest.CLIENT_ID, CredentialTest.CLIENT_SECRET, Constants.SANDBOX);
+        TransactionId transactionId = new TransactionId();
+        transactionId.setTransactionId("1555945998145");
+        PaymentCapture capture = paymentCapture.execute(apiContext,PaymentCapture.class, transactionId);
+        if(capture.getStatus() !=null && capture.getStatus().compareTo(HttpStatus.SC_OK+"")==0){
+            logger.info("Transaction");
+            logger.info(capture.getPayment().getMessage());
+            logger.info("transactio_id="+capture.getPayment().getTransaction_id());
+            logger.info("Payer="+capture.getPayment().getPayer());
+            logger.info("amount="+capture.getPayment().getCost()+"");
+        }
+
+
+
+    }
+
+    @Test
+    public void captureByOrderId() throws Exception {
+        PaymentCapture paymentCapture = new PaymentCapture();
+        APIContext apiContext = new APIContext(CredentialTest.CLIENT_ID, CredentialTest.CLIENT_SECRET, Constants.SANDBOX);
+        OrderId orderId = new OrderId();
+        orderId.setOrderId("1555952605");
+        PaymentCapture capture = paymentCapture.execute(apiContext,PaymentCapture.class, orderId);
+        if(capture.getStatus() !=null && capture.getStatus().compareTo(HttpStatus.SC_OK+"")==0){
+            logger.info("Transaction");
+            logger.info(capture.getPayment().getMessage());
+            logger.info("transactio_id="+capture.getPayment().getTransaction_id());
+            logger.info("Payer="+capture.getPayment().getPayer());
+            logger.info("amount="+capture.getPayment().getCost()+"");
+        }
+
+
+
+    }
+}
