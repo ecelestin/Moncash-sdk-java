c8d1174f7239bcc34affbc805626c0949a6a2282
emmanuelsuy <suyemmanuel;@gmaul.com>
2023-05-30 15:13:07 -0400

    remove cr

--- src/test/java/com/digicelgroup/moncash/payments/CredentialTest.java
+++ src/test/java/com/digicelgroup/moncash/payments/CredentialTest.java
@@ -2,8 +2,8 @@ package com.digicelgroup.moncash.payments;
 
 public class CredentialTest {
 
-    public static String CLIENT_ID = "c338a37a3152878b1d745db14800fadd";
+    public static String CLIENT_ID = "";
 
-    public static String CLIENT_SECRET = "JSlKjWtLgfbjQUtm12BGTFsiA4JfyNZXFeH3QQi8cfFU3o15Of3ASAMxpyvuCGpP";
+    public static String CLIENT_SECRET = "";
 
 }
